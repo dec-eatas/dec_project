bf7be5f020b419206976b6bcc490487f7e7a5d7d
zum-m <89333894+zum-m@users.noreply.github.com>
2022-02-08 16:36:25 +0900

    [modify add] Ansersのマイグレーション内容を、DB設計書を元に修正しました

--- backend/database/migrations/2022_02_08_065103_create_answers_table.php
+++ backend/database/migrations/2022_02_08_065103_create_answers_table.php
@@ -14,7 +14,22 @@ class CreateAnswersTable extends Migration
     public function up()
     {
         Schema::create('answers', function (Blueprint $table) {
-            $table->id();
+            // $table->id();
+            
+            // 外部キー制約があるのでunseined 🟡第二引数をtrueにすると自動で数が上がる、なくても同じ？
+            $table->unsignedBigInteger('id', true);
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('question_id');
+            $table->longText('content');
+            // timestampと書いてしまうと、レコード挿入時、更新時に値が入らないので、DB::rawで直接書いてます
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            // 論理削除を定義→deleted_atを自動生成
+            $table->softDeletes();
+            // 外部キー制約 user_idはusersテーブルのidが存在するものしか入らない
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('question_id')->references('id')->on('questions');
+
             $table->timestamps();
         });
     }
