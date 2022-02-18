799674b8af7ef287b8076ae0bfeb4bfa14377812
zum-m <89333894+zum-m@users.noreply.github.com>
2022-02-18 21:53:25 +0900

    [adding] ログインユーザのみの表示ではないのですが、 質問詳細に編集ボタンを追加し編集画面に遷移できるようにしました。

--- backend/resources/views/questions/show.blade.php
+++ backend/resources/views/questions/show.blade.php
@@ -7,12 +7,10 @@
         <div class="border p-4">
             <h1 class="h5 mb-4">
                 投稿の詳細
-                <form class="card-body" action="{{ route('Que.destroy') }}" method="POST">
-                    @csrf
-                    <input type="hidden" name="question_id" value="{{ $show_question['id']  }}">
-                    <button type="submit">削除</button>
-                </form>
             </h1>
+            <a href="/question/{{ $show_question['id'] }}/edit">
+                <button>編集</button>
+            </a>
 
 
             @csrf
@@ -20,12 +18,12 @@
             <input type="hidden" name="question_id" value="{{ $show_question['id']  }}">
 
             <div class="form-group">
-                <q>タイトル</q>
+                <h3>タイトル</h3>
                 <span name="title">{{$show_question['title']}}</span>
             </div>
 
             <div class="form-group">
-                <p>本文</p>
+                <h3>本文</h3>
                 <span name="content">{{$show_question['content']}}</span>
             </div>
         </div>
